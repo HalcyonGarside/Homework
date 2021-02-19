26c0d50417b5df97ed6be8ca1bc08f7952444fae
HalcyonGarside <wil.s.blanchard@gmail.com>
2021-02-18 22:08:23 -0600

    Added child processes for non-builtins - Added child processes for non-built-in command line functions - ISSUE: flags and arguments don't work

--- 308/sh/shell.c
+++ 308/sh/shell.c
@@ -8,12 +8,48 @@
 #include <sys/wait.h>
 
 //local
-//#include <process_helper.h>
+//#include <process_helper.h> 
 
 /*
- * Calls the system call specified by call and returns 
- * that process's pid.
+ * Calls the system call specified by arg and returns 
+ * the child process's status after it's run.
 */
+int runChildProc(char* arg)
+{
+	int status;
+	int proc = fork();
+	if(proc == 0)
+	{
+		char* arg0 = strtok(arg, " \n");
+		char* file = (char*)malloc(20 * sizeof(char));
+		sprintf(file, "/bin/%s", arg0);
+
+		int i = 1;
+		char* args[11];
+		args[0] = arg0;
+		while(arg0 != NULL && i < 10)
+		{
+			arg0 = strtok(NULL, " \n");
+			printf("%s ", arg0);
+			args[i] = arg0;
+			i++;
+		}
+		printf("\n");
+
+		int error = execvp(file, args); 
+
+		if(error == -1)
+		{
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		wait(&status);
+	}
+
+	return status;
+}
 
 int main(int argc, char* argv[])
 {
@@ -46,12 +82,13 @@ int main(int argc, char* argv[])
 	{
 		//Print command line
 		printf("%s> ", prompt);
-		scanf("%s", command);
+		fgets(command, 50, stdin);
 		
 		//Parse command
+		/*
 		int cmdptr = 0;
 		int argptr = 0;
-		while(cmdptr < strlen(command) && command[cmdptr] != ' ')
+		while(cmdptr < strlen(command) && command[cmdptr] != ' ' && command[cmdptr] != '\n')
 		{
 			arg[argptr] = command[cmdptr];
 			cmdptr++;
@@ -59,28 +96,33 @@ int main(int argc, char* argv[])
 		}
 		arg[argptr] = '\0';
 		argptr = 0;
+		*/
+		arg = strtok(command, " \n");
 
-		printf("%s\n", arg); //DEBUG
-
+		//printf("%s\n", arg); //DEBUG
+		
 
 		//Begin command definitions...
 		//pid
 		if(strcmp(arg, "pid") == 0)
 		{
 			printf("%d\n", getpid());
+			continue;
 		}
 		
 		//ppid
 		else if(strcmp(arg, "ppid") == 0)
 		{
 			printf("%d\n", getppid());
+			continue;
 		}
 		
 		//cd <dir>
 		else if(strcmp(arg, "cd") == 0)
 		{
+			/*
 			cmdptr++;
-			while(cmdptr < strlen(command) && command[cmdptr] != ' ')
+			while(cmdptr < strlen(command) && command[cmdptr] != ' ' && command[cmdptr] != '\n')
 			{
 				arg[argptr] = command[cmdptr];
 				cmdptr++;
@@ -88,31 +130,24 @@ int main(int argc, char* argv[])
 			}
 			arg[argptr] = '\0';
 			argptr = 0;
+			*/
 
-			printf("%s\n", arg); //DEBUG
-
+			arg = strtok(NULL, " \n");
 			int error = chdir(arg);
 
 			if(error == -1)
 			{
 				printf("Could not find path %s\n", arg);
 			}
+			continue;
 		}
 		
 		//pwd
 		else if(strcmp(arg, "pwd") == 0)
 		{
-			pid_t proc = fork();
-			int status;
-
-			if(proc == 0)
-			{
-				execl("/bin/pwd", "pwd", (char*)NULL);
-			}
-			else
-			{
-				waitpid(proc, &status, 0);
-			}
+			getcwd(arg, 50);
+			printf("%s\n", arg);
+			continue;
 		}
 		
 		//exit
@@ -124,7 +159,9 @@ int main(int argc, char* argv[])
 		//Called if no other case is taken
 		else
 		{
-			printf("Cannot exec %s: No such file or directory\n", arg);
+			runChildProc(command);
+			//printf("Cannot exec %s: No such file or directory\n", arg);
+			continue;
 		}
 
 	}
